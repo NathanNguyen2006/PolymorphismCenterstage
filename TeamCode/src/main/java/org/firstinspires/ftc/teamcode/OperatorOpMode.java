75575c02b5547762c643f9250d8a2a9dfa63fb19
Anna <annakacey@gmail.com>
2023-12-09 16:44:19 -0500

    op mode using one button for plane launcher

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OperatorOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OperatorOpMode.java
@@ -61,6 +61,7 @@ public class OperatorOpMode extends LinearOpMode{
             telemetry.addData("ArmR", roboController.ArmR.getCurrentPosition());
             telemetry.addData("ArmRpow", roboController.ArmR.getPower());
             telemetry.addData("Drone: Plane Launcher", roboController.Drone.getPosition());
+            telemetry.addData("y-stick value", armPad.left_stick_y);
             telemetry.update();
         }
     }
